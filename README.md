e7c13b5c4d35ead7c859393371b018245085cce5
alexlebens <alexanderlebens@gmail.com>
2024-04-10 02:17:51 -0600

    add initial docker file and config

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # taiga-back-docker-oidc
 
-Taiga Back Images that includes the OpenID Plugin.
+Taiga Back image that includes the OpenID Plugin.
 
 [taiga-back](https://github.com/taigaio/taiga-back)
 
 [taiga-contrib-oidc-auth](https://github.com/taigaio/taiga-contrib-oidc-auth)
+
+
+# Environment Values
+
+Use the following environmental setting to configure the taiga-back image to enable OIDC auth.
+
+```
+OIDC_ENABLED: "True"
+OIDC_SCOPES: "openid profile email"
+OIDC_SIGN_ALGO: "RS256"
+OIDC_CLIENT_ID: <generate from auth provider>
+OIDC_CLIENT_SECRET: <generate from auth provider>
+OIDC_BASE_URL: "https://id.fedoraproject.org/openidc"
+OIDC_JWKS_ENDPOINT: "https://id.fedoraproject.org/openidc/Jwks"
+OIDC_AUTHORIZATION_ENDPOINT: "https://id.fedoraproject.org/openidc/Authorization"
+OIDC_TOKEN_ENDPOINT: "https://id.fedoraproject.org/openidc/Token"
+OIDC_USER_ENDPOINT: "https://id.fedoraproject.org/openidc/UserInfo"
+```
---
+++ config/config.py
@@ -0,0 +1,21 @@
+import os
+OIDC_ENABLED = os.getenv('OIDC_ENABLED', 'False') == 'True'
+if OIDC_ENABLED:
+    INSTALLED_APPS += [
+        "mozilla_django_oidc",
+        "taiga_contrib_oidc_auth",
+    ]
+    AUTHENTICATION_BACKENDS = list(AUTHENTICATION_BACKENDS) + [
+        "taiga_contrib_oidc_auth.oidc.TaigaOIDCAuthenticationBackend",
+    ]
+    ROOT_URLCONF = "settings.urls"
+    OIDC_CALLBACK_CLASS = "taiga_contrib_oidc_auth.views.TaigaOIDCAuthenticationCallbackView"
+    OIDC_RP_SCOPES = os.getenv("OIDC_SCOPES")
+    OIDC_RP_SIGN_ALGO = os.getenv("OIDC_SIGN_ALGO")
+    OIDC_RP_CLIENT_ID = os.getenv("OIDC_CLIENT_ID")
+    OIDC_RP_CLIENT_SECRET = os.getenv("OIDC_CLIENT_SECRET")    
+    OIDC_BASE_URL = os.getenv("OIDC_BASE_URL")
+    OIDC_OP_JWKS_ENDPOINT = os.getenv("OIDC_JWKS_ENDPOINT")
+    OIDC_OP_AUTHORIZATION_ENDPOINT = os.getenv("OIDC_AUTHORIZATION_ENDPOINT")
+    OIDC_OP_TOKEN_ENDPOINT = os.getenv("OIDC_TOKEN_ENDPOINT")
+    OIDC_OP_USER_ENDPOINT = os.getenv("OIDC_USER_ENDPOINT")
